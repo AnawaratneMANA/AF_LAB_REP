0756a6be07a9928c81cce8e000b1a9e813e9f822
Nirmith Akash <it19086408@my.sliit.lk>
2021-03-28 18:58:51 +0530

    Added simple callback and fully promise coverted version of it.

---
+++ js_index_holder/SELF_STUDY/fully_promise_version_of_simple_callback.js
@@ -0,0 +1,36 @@
+//Original Method
+function first_method(){
+    value = 10;
+    //Create a Promise object and return it.
+    return new Promise(function (resolve, reject){
+        value = 10;
+        resolve(value);
+    })
+}
+
+//Second method modified with promises.
+function multi_by_11(value){
+    return new Promise(function (resolve, reject){
+        new_value = value * 10;
+        resolve(new_value);
+    }, 1000);
+
+}
+
+//Calling both functions
+first_method().then(
+    function(value){
+        return multi_by_11(value).then(
+            function(value){
+                console.log("This is the modified value - " + value);
+            }
+        )
+    });
+
+
+/**
+ * Method that call the other method will grow in a > shape. Function parameters will come inside another parameter
+ *
+ * In the Fully Promise method.
+ * Here the thing is .then can be used only with return statement that    * returns a Promise object. then -> function(){} this the nesting pattern.
+ **/
